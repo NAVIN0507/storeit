f94b5bd41fce703d65c3602d5b5774fd59897118
NAVIN0507 <navinofficial2005@gmail.com>
2024-12-20 14:07:10 +0530

    Added upload structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental:{
+    serverActions:{
+      bodySizeLimit:  '100MB'
+    },
+  },
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol:'https',
+        hostname:'img.freepik.com'
+      },
+       {
+        protocol:'https',
+        hostname:'static.vecteezy.com'
+      },
+      {
+        protocol:'https',
+        hostname:'cloud.appwrite.io'
+      }
+
+    ]
+  }
 };
 
 export default nextConfig;
